5ebac09efc4ba853f15f2adf47b43ef31fdd2085
Grusie <rbtjr02@naver.com>
2022-03-24 14:12:53 +0900

    Rect_Test

--- app/src/main/java/cf/untitled/salend/MainActivity.kt
+++ app/src/main/java/cf/untitled/salend/MainActivity.kt
@@ -1,11 +1,24 @@
 package cf.untitled.salend
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import cf.untitled.salend.databinding.ActivityMainBinding
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
+import android.view.WindowManager
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
