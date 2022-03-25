c2897e7d64f705d3da9ef454258d3653c3a9483f
Grusie <rbtjr02@naver.com>
2022-03-25 16:08:26 +0900

    design MainActivity

--- app/src/main/java/cf/untitled/salend/MainActivity.kt
+++ app/src/main/java/cf/untitled/salend/MainActivity.kt
@@ -1,24 +1,15 @@
 package cf.untitled.salend
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import cf.untitled.salend.databinding.ActivityMainBinding
-import android.view.ViewTreeObserver
-import android.view.ViewTreeObserver.OnGlobalLayoutListener
-import android.view.WindowManager
-import android.widget.LinearLayout
-import androidx.recyclerview.widget.LinearLayoutManager
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
     }
 }
--- app/src/main/java/cf/untitled/salend/RectButton.kt
+++ app/src/main/java/cf/untitled/salend/RectButton.kt
@@ -2,9 +2,9 @@ package cf.untitled.salend
 
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import android.widget.Button
 
-class RectView2: View {
+class RectButton: Button {
     constructor(context: Context?) : super(context)
     constructor(context: Context?,attrs: AttributeSet?) : super(context,attrs)
     constructor(context: Context?,attrs: AttributeSet?,defStyleAttr : Int) : super(context, attrs, defStyleAttr)
