d9cde9aecedf94888e0bd6dd9197bf543d37c3f1
Grusie <rbtjr02@naver.com>
2022-03-27 17:23:59 +0900

    add TabLayout Using ViewPager, Linking up 5 fragments. Have to modify RecyclerView in HomeFragment.kt

--- app/src/main/java/cf/untitled/salend/MainActivity.kt
+++ app/src/main/java/cf/untitled/salend/MainActivity.kt
@@ -2,14 +2,33 @@ package cf.untitled.salend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RelativeLayout
+import cf.untitled.salend.adapter.MainFragmentStatePagerAdapter
 import cf.untitled.salend.databinding.ActivityMainBinding
+import cf.untitled.salend.databinding.BottomNavigationTabBinding
 
+lateinit var binding:ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        configureBottomNavigation()
+    }
+
+    private fun configureBottomNavigation(){
+        val tabButtonBinding = BottomNavigationTabBinding.inflate(layoutInflater)
+        binding.apply {
+            vpAcMainFragPager.adapter = MainFragmentStatePagerAdapter(supportFragmentManager, 5)
+            tlAcMainBottomMenu.setupWithViewPager(vpAcMainFragPager)
+            tlAcMainBottomMenu.getTabAt(0)!!.customView = tabButtonBinding.btnBottomNaviHomeTab as RelativeLayout
+            tlAcMainBottomMenu.getTabAt(1)!!.customView = tabButtonBinding.btnBottomNaviLikeTab as RelativeLayout
+            tlAcMainBottomMenu.getTabAt(2)!!.customView = tabButtonBinding.btnBottomNaviMapTab as RelativeLayout
+            tlAcMainBottomMenu.getTabAt(3)!!.customView = tabButtonBinding.btnBottomNaviPayListTab as RelativeLayout
+            tlAcMainBottomMenu.getTabAt(4)!!.customView = tabButtonBinding.btnBottomNaviMyPageTab as RelativeLayout
+
+        }
     }
 }
