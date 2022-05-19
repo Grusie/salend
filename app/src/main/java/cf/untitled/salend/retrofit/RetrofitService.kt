f9b81a10c4b052a63f75817011e60f4f2293b7a1
grusie <grusie@github.com>
2022-05-19 12:27:46 +0900

    create function, select Location && save using SharedPreferences modify retrofit URL

--- app/src/main/java/cf/untitled/salend/retrofit/RetrofitService.kt
+++ app/src/main/java/cf/untitled/salend/retrofit/RetrofitService.kt
@@ -5,21 +5,11 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-//RetorifitService.kt
 interface RetrofitService {
 
     //GET 예제
-    @GET("posts/1")
-    fun getProductArray(): Call<ProductArray>
-
-    @GET("posts/{page}")
+    @GET("{page}")
     fun getProductArrayPage(@Path("page") page: String): Call<ProductArray>
-
-    @GET(".")
-    fun getProductArrayPage2(): Call<ProductArray>
-
-    @GET("test")
-    fun getProductArrayPage3(): Call<ProductArray>
 
 
 //    @GET("posts/1")
