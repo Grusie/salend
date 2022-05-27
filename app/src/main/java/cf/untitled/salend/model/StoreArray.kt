e612588ad6c9c08b81e21622e8a5c6da780e6bad
grusie <grusie@github.com>
2022-05-27 17:33:46 +0900

    push test

---
+++ app/src/main/java/cf/untitled/salend/model/StoreArray.kt
@@ -0,0 +1,5 @@
+package cf.untitled.salend.model
+
+data class StoreArray(
+    var stores : ArrayList<StoreData>
+)
--- app/src/main/java/cf/untitled/salend/model/StoreData.kt
+++ app/src/main/java/cf/untitled/salend/model/StoreData.kt
@@ -1,10 +1,13 @@
 package cf.untitled.salend.model
 
 data class StoreData(
-    var s_id: String,
+    var _id: String,
     var s_name: String?,
-    var s_location: String?,
+    var s_address: String?,
     var s_time: String?,
     var s_image: String?,
-    var s_tag: List<Int>?
+    var s_certiified:Boolean?,
+    var s_tag: List<Int>?,
+    var s_lat: Double?,
+    var s_lng: Double?,
 )
