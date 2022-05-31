f7f2ddcfc8a7b121e9511ef36c4c560d43e29373
Sangho Kim <rlatkd0727@naver.com>
2022-05-31 14:31:36 +0900

    add

---
+++ app/src/main/java/cf/untitled/salend/adapter/FavoriteStoreAdapter.kt
@@ -0,0 +1,21 @@
+package cf.untitled.salend.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import cf.untitled.salend.databinding.ItemKategorieBinding
+
+class FavoriteStoreAdapter : RecyclerView.Adapter<FavoriteStoreViewHoder>() {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteStoreViewHoder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: FavoriteStoreViewHoder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class FavoriteStoreViewHoder(binding : ItemKategorieBinding) : RecyclerView.ViewHolder(binding.root)
