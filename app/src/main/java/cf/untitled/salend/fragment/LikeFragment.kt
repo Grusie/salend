2a6709c716ca2ee9ef61873ac674f66eeb24264a
Sangho Kim <rlatkd0727@naver.com>
2022-06-02 22:39:13 +0900

    add

--- app/src/main/java/cf/untitled/salend/fragment/LikeFragment.kt
+++ app/src/main/java/cf/untitled/salend/fragment/LikeFragment.kt
@@ -1,60 +1,54 @@
 package cf.untitled.salend.fragment
 
 import android.os.Bundle
+import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import cf.untitled.salend.MyApplication
 import cf.untitled.salend.R
+import cf.untitled.salend.databinding.FragmentLikeBinding
+import cf.untitled.salend.databinding.FragmentMyPageBinding
+import cf.untitled.salend.model.StoreArray
+import cf.untitled.salend.model.StoreData
+import cf.untitled.salend.retrofit.RetrofitService
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.concurrent.thread
 
-// TODO: Rename parameter arguments, choose names that match
-// the cf.untitled.salend.fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LikeFragment.newInstance] factory method to
- * create an instance of this cf.untitled.salend.fragment.
- */
 class LikeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var binding: FragmentLikeBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this cf.untitled.salend.fragment
-        return inflater.inflate(R.layout.fragment_like, container, false)
+        binding = FragmentLikeBinding.inflate(layoutInflater)
+
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.salend.tk")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(RetrofitService::class.java)
+
+
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this cf.untitled.salend.fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of cf.untitled.salend.fragment LikeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LikeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
