03fa09191831a0f650056de0f59f72479419930a
Sangho Kim <rlatkd0727@naver.com>
2022-06-07 01:07:02 +0900

    add

--- app/src/main/java/cf/untitled/salend/interface2/IStoreService.kt
+++ app/src/main/java/cf/untitled/salend/interface2/IStoreService.kt
@@ -1,10 +1,10 @@
 package cf.untitled.salend.interface2
 
-import cf.untitled.salend.model.KategoriStore
+import cf.untitled.salend.model.CategoryStore
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface IStoreService {
     @GET("store")
-    fun stores() : Call<KategoriStore>
+    fun stores() : Call<CategoryStore>
 }
---
+++ app/src/main/java/cf/untitled/salend/model/ProductArray2.kt
@@ -0,0 +1,6 @@
+package cf.untitled.salend.model
+
+data class ProductArray2(
+    var items: ArrayList<ProductData>
+)
+
