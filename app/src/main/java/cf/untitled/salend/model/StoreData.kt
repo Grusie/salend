6039a8faeb13629e6f7d7c8ac23bd78b154e90a1
Grusie <rbtjr02@naver.com>
2022-08-12 19:07:13 +0900

    Modify design fragment_map, include store img in Balloon msg

--- app/src/main/java/cf/untitled/salend/model/StoreData.kt
+++ app/src/main/java/cf/untitled/salend/model/StoreData.kt
@@ -6,7 +6,7 @@ data class StoreData(
     var s_address: String?,
     var s_time: String?,
     var s_image: String?,
-    var s_certiified:Boolean?,
+    var s_certified:Boolean?,
     var s_tag: List<Int>?,
     var s_lat: Double?,
     var s_lng: Double?,
