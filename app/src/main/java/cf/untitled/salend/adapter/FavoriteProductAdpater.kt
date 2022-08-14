bc928e5832472686f7aadbbb2cde0bf1c08f7b07
koreaksh <rlatkd0727@naver.com>
2022-08-15 00:45:32 +0900

    찜 상품 UI 변경
    변경

--- app/src/main/java/cf/untitled/salend/adapter/FavoriteProductAdpater.kt
+++ app/src/main/java/cf/untitled/salend/adapter/FavoriteProductAdpater.kt
@@ -1,6 +1,7 @@
 package cf.untitled.salend.adapter
 
 import android.content.Intent
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
@@ -8,7 +9,9 @@ import androidx.recyclerview.widget.RecyclerView
 import cf.untitled.salend.ProductActivity
 import cf.untitled.salend.StoreChoiceActivity
 import cf.untitled.salend.databinding.ItemCategoryBinding
+import cf.untitled.salend.databinding.ItemNearbySaleBinding
 import cf.untitled.salend.databinding.ItemStoreItemBinding
+import cf.untitled.salend.databinding.LikeItemPageBinding
 import cf.untitled.salend.model.ProductArray
 import cf.untitled.salend.model.ProductData
 import com.bumptech.glide.Glide
@@ -22,7 +25,7 @@ class FavoriteProductAdpater : RecyclerView.Adapter<FavoriteProductAdpater.Holde
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteProductAdpater.Holder {
-        val binding = ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent , false)
+        val binding = LikeItemPageBinding.inflate(LayoutInflater.from(parent.context), parent , false)
         return Holder(binding).apply {
             itemView.setOnClickListener{
                 val curPos = adapterPosition
@@ -41,12 +44,16 @@ class FavoriteProductAdpater : RecyclerView.Adapter<FavoriteProductAdpater.Holde
         holder.setData(item)
     }
 
-    inner class Holder(val binding : ItemCategoryBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class Holder(val binding : LikeItemPageBinding) : RecyclerView.ViewHolder(binding.root) {
         fun setData(data : ProductData) {
-            binding.categoryStoreName.text = data.i_name
             Glide.with(binding.root)
                 .load(data.i_image)
-                .into(binding.categoryStoreImage)
+                .into(binding.likeItemPageImg)
+            binding.likeItemPageStoreName.text = data.i_store_name
+            binding.likeItemPageProductName.text = data.i_name
+            binding.likeItemPageProductPrice.text = "${data.i_price.toString()}￦"
+            binding.likeItemPageProductPrice.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG)
+            binding.likeItemPageProductNowPrice.text = "${data.i_now_price.toString()}￦"
         }
     }
 }
